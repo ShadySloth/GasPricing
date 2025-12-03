ecf1753af78934a46687ffeb8b4d57ff33895b71
Peder Antonisen <113884778+ShadySloth@users.noreply.github.com>
2025-12-03 14:54:44 +0100

    feat: unit tests
    Signed-off-by: Peder Antonisen <peder_97@hotmail.com>

--- Tests/UnitTests/CalculateGasPriceTests.cs
+++ Tests/UnitTests/CalculateGasPriceTests.cs
@@ -1,4 +1,6 @@
-﻿using Tests.Util.Fixtures;
+﻿using API.Application.Services;
+using API.Domain.DTOs;
+using Tests.Util.Fixtures;
 
 namespace Tests.UnitTests;
 
@@ -13,7 +15,112 @@ public class CalculateGasPriceTests : IClassFixture<GasFixture>
 
     #region CalculateGasPrice Tests
 
-    // TODO: tests for CalculateGasPrice
+    [Theory]
+    [InlineData("Regular", 10, true)]
+    public async Task CalculateGasPrice_ReturnsTotalPrice_Correctly(string gasTypeName, int liters, bool membership)
+    {
+        // Arrange
+        var refuelDto = new RefuelDto
+        {
+            GasTypeName = gasTypeName,
+            Liters = liters,
+            Membership = membership
+        };
+        
+        var gasPrice = gasTypeName switch
+        {
+            "Regular" => 12.70,
+            "Premium" => 14.50,
+            "Diesel" => 7.20,
+            _ => 1
+        };
+        
+        var discountPercentage = 1.00;
+        if (membership)
+        {
+            discountPercentage = 0.90;
+        }
+        
+        discountPercentage = liters switch
+        {
+            <= 20 => discountPercentage,
+            <= 50 => discountPercentage - 0.05,
+            <= 100 => discountPercentage - 0.10,
+            > 100 => discountPercentage - 0.15
+        };
+
+        var expectedPrice = liters * gasPrice * discountPercentage;
+
+        var gasService = new GasService(_fixture.GasTypeRepositoryMock.Object);
+        
+        // Act
+        var result = await gasService.CalculateGasPrice(refuelDto);
+        
+        // Assert
+        Assert.Equal(expectedPrice, result.TotalPrice);
+    }
+    
+    [Theory]
+    [InlineData("Regular", -10, false)]
+    [InlineData("Premium", 0, true)]
+    public async Task CalculateGasPrice_ThrowsArgumentOutOfRangeException_ForInvalidLiters(string gasTypeName, int liters, bool membership)
+    {
+        // Arrange
+        var refuelDto = new RefuelDto
+        {
+            GasTypeName = gasTypeName,
+            Liters = liters,
+            Membership = membership
+        };
+
+        var gasService = new GasService(_fixture.GasTypeRepositoryMock.Object);
+        
+        // Act
+        Task result() => gasService.CalculateGasPrice(refuelDto);
+        
+        // Assert
+        await Assert.ThrowsAsync<ArgumentOutOfRangeException>(result);
+    }
+    
+    [Fact]
+    public async Task CalculateGasPrice_ThrowsArgumentException_ForInvalidGasType()
+    {
+        // Arrange
+        var refuelDto = new RefuelDto
+        {
+            GasTypeName = "Milk",
+            Liters = 10,
+            Membership = true
+        };
+
+        var gasService = new GasService(_fixture.GasTypeRepositoryMock.Object);
+        
+        // Act
+        Task result() => gasService.CalculateGasPrice(refuelDto);
+        
+        // Assert
+        await Assert.ThrowsAsync<ArgumentException>(result);
+    }
+    
+    [Fact]
+    public async Task CalculateGasPrice_ThrowsArgumentNullException_ForNullGasTypeName()
+    {
+        // Arrange
+        var refuelDto = new RefuelDto
+        {
+            GasTypeName = null,
+            Liters = 10,
+            Membership = false
+        };
+
+        var gasService = new GasService(_fixture.GasTypeRepositoryMock.Object);
+        
+        // Act
+        Task result() => gasService.CalculateGasPrice(refuelDto);
+        
+        // Assert
+        await Assert.ThrowsAsync<ArgumentNullException>(result);
+    }
 
     #endregion
 }
