8d0b462b3a21d4973fb347fc1d2f8aae486789a3
ShadySloth <peder_97@hotmail.com>
2025-12-15 12:25:34 +0100

    refactor: docs on refuelDto

--- API/Domain/DTOs/RefuelDto.cs
+++ API/Domain/DTOs/RefuelDto.cs
@@ -2,7 +2,16 @@
 
 public class RefuelDto
 {
+    /**
+     * The number of liters to refuel.
+     */
     public required int Liters { get; set; }
+    /**
+     * The type of gas to refuel.
+     */
     public required string GasTypeName { get; set; }
+    /**
+     * Membership status for start discount.
+     */
     public required bool Membership { get; set; }
 }
